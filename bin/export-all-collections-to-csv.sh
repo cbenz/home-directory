c72a061f2cedd746c9ec9f289c87ad1f4023ab55
Christophe Benz <cbenz@easter-eggs.com>
2013-12-02 15:17:23 +0100

    Add script.

---
+++ bin/export-all-collections-to-csv.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# From http://stackoverflow.com/questions/8949335/mongo-export-all-fields-data-from-collection-without-specifying-fields
+
+
+OIFS=$IFS;
+IFS=",";
+
+dbname=$1 #put "database name" here if you don't want to pass it as an argument
+
+collections=`mongo $dbname --eval "rs.slaveOk();db.getCollectionNames();" --quiet`;
+collectionArray=($collections);
+
+for ((i=0; i<${#collectionArray[@]}; ++i));
+do
+    keys=`mongo $dbname --eval "rs.slaveOk();var keys = []; for(var key in db.${collectionArray[$i]}.findOne()) { keys.push(key); }; keys;" --quiet`;
+    mongoexport --db $dbname --collection ${collectionArray[$i]} --fields "$keys" --csv --out $dbname.${collectionArray[$i]}.csv;
+done
+
+IFS=$OIFS;
